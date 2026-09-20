fef377ddb679ac836ef6bc631b18fca3fdd44d64
NarasingaRao Reddi <narasingarao981@gmail.com>
2026-09-20 12:42:09 +0530

    ochestration setup

--- Orchestrator.sh
+++ Orchestrator.sh
@@ -5,28 +5,9 @@ AMI_ID="ami-0220d79f3f480ecf5"
 SG_GROUP="sg-0f20ff58735ff6cde"
 instance_type="t3.micro"
 subnet_id="subnet-03389726b7cd3df4e"
-CURRENT_PATH=$PWD
-CSV_PATH="$CURRENT_PATH/ips.csv"
+CSV_PATH="$PWD/ips.csv"
 HOSTED_ZONE_ID="Z04148142KCH92IOUT66N"
 DOMAIN_NAME="singamden.fun"
-
-
-#function to create instances and update DNS records
-create_instance(){
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" --instance-type $instance_type --security-group-ids $SG_GROUP --subnet-id $subnet_id --query 'Instances[0].InstanceId' --output text)
-    
-    if [ $1 == "frontend" ]
-    then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
-        RECORD_NAME="$1.$DOMAIN_NAME"
-        update_dns_route53 $RECORD_NAME $IP
-    else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
-        RECORD_NAME="$1.$DOMAIN_NAME"
-        update_dns_route53 $RECORD_NAME $IP
-    fi
-    
-}
 
 #function to update DNS records with instance IP addresses
 update_dns_route53(){
@@ -55,18 +36,28 @@ update_dns_route53(){
 }
 
 
+#function to create instances and update DNS records
+create_instance(){
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" --instance-type $instance_type --security-group-ids $SG_GROUP --subnet-id $subnet_id --query 'Instances[0].InstanceId' --output text)
+    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+    if [ $1 == "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+        RECORD_NAME="$1.$DOMAIN_NAME"
+        update_dns_route53 $RECORD_NAME $IP
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+        RECORD_NAME="$1.$DOMAIN_NAME"
+        update_dns_route53 $RECORD_NAME $IP
+    fi
+    
+}
 
-if [ -f $CSV_PATH ]
-then
-    rm -rf $CSV_PATH
-    touch $CSV_PATH
-    chmod 777 $CSV_PATH
-else
-    touch $CSV_PATH
-    chmod 777 $CSV_PATH
-fi
 
-for instance in instances 
+
+: > "$CSV_PATH"
+
+for instance in "${instances[@]}"
 do 
     create_instance instance
 done
