62714b45a7f0ad006504aa91d9c22750c00149c3
NarasingaRao Reddi <narasingarao981@gmail.com>
2026-09-20 16:31:15 +0530

    ochestration setup

--- ConfigureApplication.sh
+++ ConfigureApplication.sh
@@ -9,6 +9,6 @@ PASSWD="DevOps321"
 
 file="/home/ec2-user/roboshop-shell/ips.csv"
 while read line; do
-  #InstanceName= $(echo "$line" | cut -d " " -f 1)
-  echo $line
+  InstanceName=$(echo "$line" | cut -d " " -f 1)
+  echo $InstanceName
 done < "$file"
