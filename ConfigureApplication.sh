f1a9cd345ba234707bcffaedb4cd88a2944f1f29
NarasingaRao Reddi <narasingarao981@gmail.com>
2026-09-20 19:44:09 +0530

    ochestration setup

--- ConfigureApplication.sh
+++ ConfigureApplication.sh
@@ -2,16 +2,17 @@
 
 USER="ec2-user"
 PASSWD="DevOps321"
-
-# Source - https://stackoverflow.com/a/9587786
-# Posted by Benoit, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-09-20, License - CC BY-SA 4.0
-
 file="/home/ec2-user/roboshop-shell/ips.csv"
+
+#1.reads the instance IPs
+#2.Connect to the Instance with the IP and credentials
+#3.Execute the respective script
 while read line; do
+
   InstanceName=$(echo "$line" | cut -d " " -f 1)
   IP=$(echo "$line" | cut -d " " -f 3)
   script="$InstanceName.sh"
+
   echo "Connecting to $InstanceName"
   sshpass -p "$PASSWD" ssh -o StrictHostKeyChecking=no $USER@$IP "[ ! -d 'roboshop-shell' ] && git clone https://github.com/narasingarao981/roboshop-shell.git;cd /home/ec2-user/roboshop-shell; sh $script" < /dev/null
 
